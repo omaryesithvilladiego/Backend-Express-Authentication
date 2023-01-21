fc371901619a5d771e2d4f6c9c3a6c4f48f8d34b
omaryesithvilladiego <123215875+omaryesithvilladiego@users.noreply.github.com>
2023-01-21 15:29:19 -0500

    configurando servidor web

--- app.js
+++ app.js
@@ -6,3 +6,53 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+//Mongoose configuration
+mongoose.set('strictQuery', false);
+
+
+
+//Api port
+
+const PORT = 9000
+
+app.listen(PORT, () => {
+    console.log("app running in port" + PORT)
+})
+
+app.use("/", (req, res) => {
+    res.send("Hi")
+
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
+  module.exports = app;
